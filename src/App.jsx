56c44f2eb5793b6b7da09f5e934f5af81887620e
Xavi Guasch <xaviguasch@gmail.com>
2026-03-14 08:14:33 +0100

    added Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 
 import "./App.css";
 import Trends from "./components/Trends";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="pt-8 pb-20 px-4 ">
+      <Hero />
       <Trends />
     </div>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+function Hero() {
+  return (
+    <div className="flex flex-col gap-12 items-center">
+      <div className="flex flex-col gap-4 items-center">
+        <h2 className="text-bright-blue-text font-bold text-2xl">
+          Hello, Lisa!
+        </h2>
+        <h1 className="text-preset-1-mobile text-center">
+          How are you feeling today?
+        </h1>
+
+        <span className="text-preset-6 text-light-grey-text">
+          Wednesday, April 16th, 2025
+        </span>
+      </div>
+
+      <button className="text-preset-5 text-white-text bg-bright-blue-btn px-8 py-4 rounded-[10px]">
+        Log today's mood
+      </button>
+    </div>
+  );
+}
+
+export default Hero;
