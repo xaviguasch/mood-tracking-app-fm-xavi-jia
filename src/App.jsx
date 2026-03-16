9d0788b1e51e7108a92c19c4f794a128de445c5b
Xavi Guasch <xaviguasch@gmail.com>
2026-03-16 15:20:07 +0100

    added Header on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { useState } from "react";
 import "./App.css";
 import Trends from "./components/Trends";
 import Hero from "./components/Hero";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="pt-8 pb-20 px-4 ">
+      <Header />
       <Hero />
       <Trends />
     </div>
