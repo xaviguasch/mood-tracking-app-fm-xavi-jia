b9ad0bf1e02b2348c5fce27af9daac92bba1a095
Xavi Guasch <xaviguasch@gmail.com>
2026-03-19 08:18:56 +0100

    added last changes to Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -34,17 +34,55 @@ const MOODS = [
   },
 ];
 
+const HOURS_OF_SLEEP = [
+  "9+ hours",
+  "7-8 hours",
+  "5-6 hours",
+  "3-4 hours",
+  "0-2 hours",
+];
+
+const INITIAL_FEELINGS = {
+  Joyful: false,
+  Down: false,
+  Anxious: false,
+  Calm: false,
+  Excited: false,
+  Frustrated: false,
+  Lonely: false,
+  Grateful: false,
+  Overwhelmed: false,
+  Motived: false,
+  Irritable: false,
+  Peaceful: false,
+  Tired: false,
+  Hopeful: false,
+  Confident: false,
+  Stressed: false,
+  Content: false,
+  Disappointed: false,
+  Optimistic: false,
+  Restless: false,
+};
+
 const TOTAL_STEPS = 4;
 
 function Modal({ onIsModalOpen }) {
   const [step, setStep] = useState(1);
   const [currentMood, setCurrentMood] = useState(MOODS[0].title);
+  const [feelings, setFeelings] = useState(INITIAL_FEELINGS);
+  const [text, setText] = useState("");
+  const [selectedHours, setSelectedHours] = useState(HOURS_OF_SLEEP[0]);
+
+  const feelingsList = Object.keys(INITIAL_FEELINGS);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
     const nextStep = step + 1;
 
     if (nextStep === TOTAL_STEPS + 1) {
+      // PENDING on addding the logic for sending all the state data to the backend or somewhere else
+      onIsModalOpen();
       return;
     }
 
@@ -75,10 +113,9 @@ function Modal({ onIsModalOpen }) {
           </div>
         </header>
 
-        <h2 className="text-preset-3-mobile">How was your mood today?</h2>
-
         {step === 1 && (
-          <section>
+          <section className="flex flex-col justify-start gap-10">
+            <h2 className="text-preset-3-mobile">How was your mood today?</h2>
             <ul className="flex flex-col justify-start items-stretch gap-3">
               {MOODS.map((mood) => {
                 return (
@@ -115,12 +152,109 @@ function Modal({ onIsModalOpen }) {
           </section>
         )}
 
-        {step === 2 && <section></section>}
-        {step === 3 && <section></section>}
+        {step === 2 && (
+          <section className="flex flex-col justify-start gap-6">
+            <div>
+              <h2 className="text-preset-3-mobile">How did you feel?</h2>
+              <p>Select up to three tags:</p>
+            </div>
+            <ul className="flex flex-wrap justify-start items-stretch content-start gap-x-4 gap-y-3">
+              {feelingsList.map((option) => (
+                <div
+                  key={option}
+                  className={`px-4 py-3 text-preset-6-regular flex item gap-2 bg-white-bg rounded-[10px] border-2    ${
+                    feelings[option] === true
+                      ? "border-dark-blue"
+                      : "border-very-light-blue"
+                  }`}
+                >
+                  <input
+                    type="checkbox"
+                    id={option}
+                    value={option}
+                    checked={feelings[option] === true}
+                    onChange={(event) => {
+                      setFeelings({
+                        ...feelings,
+                        [option]: event.target.checked,
+                      });
+                    }}
+                  />
+                  <label htmlFor={option}>{option}</label>
+                </div>
+              ))}
+            </ul>
+          </section>
+        )}
+
+        {step === 3 && (
+          <section className="flex flex-col justify-start gap-6">
+            <div>
+              <h2 className="text-preset-3-mobile">Write about your day...</h2>
+            </div>
+
+            <div className="flex flex-col justify-start items-start gap-2">
+              <textarea
+                id="comment-field"
+                className="border border-light-grey w-full rounded-[10px] min-h-37.5 px-4 py-3 text-preset-6-italic text-light-grey-text"
+                placeholder="Today, I felt..."
+                value={text}
+                onChange={(event) => {
+                  setText(event.target.value);
+                }}
+              />
+              <span className="text-right w-full text-preset-8 text-light-grey-text">
+                {text.length} / 150
+              </span>
+            </div>
+          </section>
+        )}
+
+        {step === 4 && (
+          <section className="flex flex-col justify-start gap-6">
+            <div>
+              <h2 className="text-preset-3-mobile">
+                How many hours did you sleep last night?
+              </h2>
+            </div>
+
+            <ul className="flex flex-col justify-start items-stretch gap-3">
+              {HOURS_OF_SLEEP.map((hours) => {
+                return (
+                  <li key={hours}>
+                    <label
+                      htmlFor={hours}
+                      className={`flex items-center justify-between px-5 py-3 rounded-xl cursor-pointer  border-2 bg-white-bg
+                        ${
+                          hours === selectedHours
+                            ? "border-dark-blue"
+                            : "border-very-light-blue"
+                        }`}
+                    >
+                      <div className="flex items-center gap-3">
+                        <input
+                          type="radio"
+                          name="hours"
+                          value={hours}
+                          id={hours}
+                          checked={hours === selectedHours}
+                          onChange={(e) => setSelectedHours(e.target.value)}
+                          className="cursor-pointer"
+                        />
+
+                        <span>{hours}</span>
+                      </div>
+                    </label>
+                  </li>
+                );
+              })}
+            </ul>
+          </section>
+        )}
 
         <section className="">
-          <button className="bg-dark-blue text-white-text rounded-[10px] py-4 text-preset-4 w-full">
-            Continue
+          <button className="bg-dark-blue text-white-text rounded-[10px] py-4 text-preset-4 w-full cursor-pointer">
+            {step === TOTAL_STEPS ? "Submit" : "Continue"}
           </button>
         </section>
       </form>
