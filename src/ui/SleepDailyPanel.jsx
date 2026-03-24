33ffc279597f33ee87fa45e30ba54bb6a8fd5731
Xavi Guasch <xaviguasch@gmail.com>
2026-03-24 11:39:48 +0100

    added styling for SleepDailyPanel, svg issue pending

--- src/ui/SleepDailyPanel.jsx
+++ src/ui/SleepDailyPanel.jsx
@@ -1,9 +1,16 @@
+import sleepIcon from "../assets/images/icon-sleep.svg";
+
 function SleepDailyPanel({ sleepHours }) {
   return (
-    <div>
-      <p>Sleep</p>
-
-      <p>{sleepHours} hours</p>
+    <div className="flex flex-col justify-start items-start gap-4 bg-white rounded-2xl border border-translucid-line px-4 py-8">
+      <div className="flex justify-start items-center gap-3">
+        {/* temporary background, SVG issues */}
+        <div className="bg-red-500">
+          <img src={sleepIcon} alt="" />
+        </div>
+        <p className="text-preset-6 text-light-grey-text">Sleep</p>
+      </div>
+      <p className="text-preset-hours">{sleepHours} hours</p>
     </div>
   );
 }
