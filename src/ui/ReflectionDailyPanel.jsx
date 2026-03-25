aab41aa3c82f9640b7c19f31846ff50d9bedcc27
Xavi Guasch <xaviguasch@gmail.com>
2026-03-25 06:39:35 +0100

    added styling to ReflectionDailyPanel component

--- src/ui/ReflectionDailyPanel.jsx
+++ src/ui/ReflectionDailyPanel.jsx
@@ -1,11 +1,16 @@
+import reflectionIcon from "../assets/images/icon-reflection.svg";
+
 function ReflectionDailyPanel({ journalEntry, feelings }) {
   return (
-    <div>
-      <p>Relfection of the day</p>
-
-      <p>{journalEntry}</p>
-
-      <div>
+    <div className="flex flex-col justify-start items-start gap-4 bg-white rounded-2xl border border-translucid-line px-5 py-5">
+      <div className="flex justify-start items-center gap-3">
+        <img src={reflectionIcon} alt="" />
+        <p className="text-preset-6 text-light-grey-text">
+          Relfection of the day
+        </p>
+      </div>
+      <p className="text-dark-text text-preset-6">{journalEntry}</p>
+      <div className="text-preset-6-italic text-light-gray gap-3 flex ">
         {feelings.map((feeling) => (
           <span key={feeling}>#{feeling}</span>
         ))}
