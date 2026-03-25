fa5fb2e604b31eaaf838c679bdffd5cf5b73b14a
Xavi Guasch <xaviguasch@gmail.com>
2026-03-25 10:32:16 +0100

    solved sleep icon issue in SleepDailyPanel

--- src/ui/SleepDailyPanel.jsx
+++ src/ui/SleepDailyPanel.jsx
@@ -1,13 +1,11 @@
-import sleepIcon from "../assets/images/icon-sleep.svg";
+import sleepIcon from "../assets/images/icon-sleep-figma.png";
 
 function SleepDailyPanel({ sleepHours }) {
   return (
     <div className="flex flex-col justify-start items-start gap-4 bg-white rounded-2xl border border-translucid-line px-5 py-5 lg:h-full">
       <div className="flex justify-start items-center gap-3">
         {/* temporary background, SVG issues */}
-        <div className="bg-red-500">
-          <img src={sleepIcon} alt="" />
-        </div>
+        <img src={sleepIcon} alt="" />
         <p className="text-preset-6 text-light-grey-text">Sleep</p>
       </div>
       <p className="text-preset-hours">{sleepHours} hours</p>
